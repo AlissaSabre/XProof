9d76a5c20eb3c477f62bef1aad0beae2e4b456f7
alissa <alissa_sabre@yahoo.co.jp>
2016-06-04 23:14:03 +0900

    Command line version.

---
+++ XProofCmd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using XProof;
+
+namespace XProofCmd
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Out.WriteLine("Invalid command line parameters.  Usage: XProofCmd filename...");
+                return 33;
+            }
+
+            try
+            {
+                using (var proof = new Proofreader())
+                {
+                    proof.DoProof(args);
+                }
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.ToString());
+                return 1;
+            }
+        }
+    }
+}

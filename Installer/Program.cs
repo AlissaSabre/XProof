54206b1de6f7ea7f55e4174ccc2df16d49b79d02
alissa <alissa_sabre@yahoo.co.jp>
2016-06-05 13:25:27 +0900

    Fixed several bugs in installer creation.

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Installer
 {
     class Program
     {
-        static void Main(string[] args)
+        // Renames a WiX created msi file to some meaningful name with main file version embedded.
+
+        static int Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.Out.WriteLine("Usage: Installer setup.msi mainfile.exe");
+                return 33;
+            }
+            try
+            {
+                var msi = args[0];
+                var msiname = Path.GetFileName(msi);
+                var mainfile = args[1];
+                var basename = Path.GetFileNameWithoutExtension(mainfile);
+                var version = FileVersionInfo.GetVersionInfo(mainfile).FileVersion;
+                var targetname = string.Format("{0}-{1}-{2}", basename, version, msiname);
+                var target = Path.Combine(Path.GetDirectoryName(msi), targetname);
+                File.Delete(target);
+                File.Move(msi, target);
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+                return 1;
+            }
         }
     }
 }
--- Installer/wix.bat
+++ Installer/wix.bat
@@ -4,7 +4,6 @@ echo off
 set PATH=%WIX%bin;%PATH%
 set TARGET=XProof-X.X.X-setup.msi
 cd "%1"bin\Release
-candle ..\..\installer.wsx
-light installer.wixobj
-if exist "%TARGET%" del "%TARGET%"
-ren installer.msi "%TARGET%"
+candle ..\..\setup.wsx
+light setup.wixobj
+Installer setup.msi XProof.exe

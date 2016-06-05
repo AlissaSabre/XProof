b7ff13e7d912691a54cda9fbd71bc42597f4988f
alissa <alissa_sabre@yahoo.co.jp>
2016-06-05 11:02:05 +0900

    Some refinment as well as commenting as a public class.

--- XProof/Proofreader.cs
+++ XProof/Proofreader.cs
@@ -6,10 +6,19 @@ using System.Text;
 
 namespace XProof
 {
+    /// <summary>
+    /// A sort of a wrapper of Microsoft Word to proofread (spell checking and grammer checking) XLIFF documents. 
+    /// </summary>
     public class Proofreader : IDisposable
     {
         private readonly List<string> TempFilenames = new List<string>();
 
+        /// <summary>
+        /// Implements <see cref="IDisposable.Dispose"/>.
+        /// </summary>
+        /// <remarks>
+        /// This method deletes all temporary files that this object has created and still remain.
+        /// </remarks>
         public void Dispose()
         {
             foreach (var f in TempFilenames)
@@ -20,8 +29,26 @@ namespace XProof
                 }
                 catch (Exception) { }
             }
+            TempFilenames.Clear();
         }
 
+        /// <summary>
+        /// Creates an instance.
+        /// </summary>
+        public Proofreader()
+        {
+            // Do nothing special.
+        }
+
+        /// <summary>
+        /// Proofreads target contents of XLIFF files.
+        /// </summary>
+        /// <param name="filenames">An array of filesnames to proofread.
+        /// A file can be an XLIFF file or a zipped set of XLIFF files.
+        /// Those files other than XLIFF are ignored in ZIP, 
+        /// so you can pass XLIFF based packages like wsxz.
+        /// </param>
+        /// <exception cref="ApplicationException">Microsoft Word is not available on the system.</exception>
         public void DoProof(string[] filenames)
         {
             var tmp = Path.GetTempFileName();
@@ -39,9 +66,20 @@ namespace XProof
             }
             tr.SaveTo(tmp);
 
-            using (var word = new Word())
+            try
             {
-                word.Proofread(tmp);
+                using (var word = new Word())
+                {
+                    word.Proofread(tmp);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                if (e.Message == "progId not found. Word.Application")
+                {
+                    throw new ApplicationException("Microsoft Word is not available.", e);
+                }
+                throw e;
             }
         }
     }
